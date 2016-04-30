3e491ceba48a2bd515ebedaba0673a2d18ec4568
irishmann313 <bmann@nd.edu>
2016-04-30 18:39:54 -0400

    Switched networking from Protocol to LineReceiver, as online sources seem to indicate this is a better way to do pygame

--- sample.py
+++ sample.py
@@ -1,36 +1,53 @@
-from twisted.internet.protocol import Factory
 from twisted.internet.protocol import ClientFactory
-from twisted.internet.protocol import Protocol
+from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
+from twisted.internet.task import LoopingCall
+
+import pygame
 
 SERVER_HOST = "localhost"
 SERVER_PORT = 40025
 
-class ClientConnection(Protocol):
+class ClientProtocol(LineReceiver):
 
-	def __init__(self):
-		pass
+	def __init__(self, recv):
+		self.recv = recv
 
-	def dataReceived(self, data):
-		print data
-		self.transport.loseConnection()
+	def lineReceived(self, line):
+		self.recv(line)
 
-	def connectionMade(self):
-		print "connection made to %s port %s" % (SERVER_HOST, SERVER_PORT)
-		self.transport.write("hi")
+class ClientFactory(ClientFactory):
 
-	def connectionLost(self, reason):
-		print "connection lost to %s port %s" % (SERVER_HOST, SERVER_PORT)
-
-		reactor.stop()
-
-class ClientConnFactory(ClientFactory):
-
-	def __init__(self):
-		pass
+	def __init__(self, recv):
+		self.protocol = ClientProtocol
+		self.recv = recv
 
 	def buildProtocol(self, addr):
-		return ClientConnection()
+		return ClientProtocol(self.recv)
 
-reactor.connectTCP(SERVER_HOST, SERVER_PORT, ClientConnFactory())
-reactor.run()
+class Client(object):
+
+	def __init__(self):
+		self.line = 'no message'
+		pygame.init()
+		self.screen = pygame.display.set_mode((200, 200))
+
+	def new_line(self, line):
+		self.line = line
+
+	def tick(self):
+		self.screen.fill((0,0,0))
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				reactor.stop()
+		self.screen.blit(pygame.font.SysFont('mono', 12, bold=True).render(self.line, True, (0, 255, 0)), (20,20))
+		pygame.display.flip()
+
+if __name__ == '__main__':
+	c = Client()
+
+	lc = LoopingCall(c.tick)
+	lc.start(1.0/60)
+	reactor.connectTCP(SERVER_HOST, SERVER_PORT, ClientFactory(c.new_line))
+	reactor.run()
+	lc.stop()
